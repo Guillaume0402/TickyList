174f6c57a508f5f5b1d039c945daceae1ad4294b
Guillaume0402 <g.maignaut@gmail.com>
2026-02-28 14:31:02 +0100

    refactor: remove projectTasks from HomeController

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -60,16 +60,5 @@ final class HomeController extends AbstractController
             'error' => null,
             'old' => ['email' => ''],
         ]);
-    }
-
-   
-
-    public function projectTasks(): string
-    {
-        return $this->render('pages/project-task', [
-            'pageTitle' => 'Projets & Tâches',
-            'title' => 'Projets & Tâches',
-            'subtitle' => 'Page projet-tasks OK',
-        ]);
-    }
+    }  
 }
